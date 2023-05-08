4fdac489727965da0361c136a06e9dd54d7fbea7
Matias Gonzalez <MatiasG2001@outlook.com>
2023-05-07 22:09:53 -0300

    arreglamos nombre del proyecto para que levante la app

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   get 'users/new'
   get 'users/create'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
 
-RegistrationSystem::Application.routes.draw do
+Veterinaria::Application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   post 'users', to: 'users#create'
 end
