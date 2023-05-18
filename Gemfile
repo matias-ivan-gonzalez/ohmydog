b97163b4213716feedd43fcb7477a1e0b1b7ce56
Matias <matias@Debian.myguest.virtualbox.org>
2023-05-17 22:04:46 -0300

    envio de bootstrap instalado y navbar basico

--- Gemfile
+++ Gemfile
@@ -9,6 +9,11 @@ gem "rails", "~> 7.0.4", ">= 7.0.4.3"
 # The original asset pipeline for Rails [https://github.com/rails/sprockets-rails]
 gem "sprockets-rails"
 
+gem 'bootstrap'
+
+gem 'jquery-rails'
+
+
 # Use sqlite3 as the database for Active Record
 gem "sqlite3", "~> 1.4"
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  root 'home#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'registrations#new'
+  post 'signup', to: 'registrations#create'
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
